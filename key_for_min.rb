40dba3a3c841ae27f6aea77d3cdba53e236dc0a8
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-27 04:53:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,6 @@
 def key_for_min_value(hash)
 hash = {:iphone => 300, :samsung => 555, :nokia => 1000}
 hash.each do |phone, price| 
-  puts "this #{phone} is #{price}
+  puts "this #{phone} is #{price}"
 key_for_the_min_value(hash)
 end
