19bcdc46d6f2d4ffea11433bd3bdb3c52d2e318d
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-27 04:55:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
+def key_for_min_value
 hash = {:iphone => 300, :samsung => 555, :nokia => 1000}
 hash.each do |phone, price| 
   puts "this #{phone} is #{price}"
