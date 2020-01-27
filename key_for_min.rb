fd57c114409c60e1c42d9346f9b62e4eccbf041d
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-27 05:28:29 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+hash = {:iphone => 300, :samsung => 555, :nokia => 1000}
+hash.each.collect do |price| 
+  puts "this phone is #{price}"
 end
