cc166f9017b4630b2d082201f7e36cf5e233d662
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-27 05:14:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-hash = {:iphone => 300 :samsung => 555 :nokia => 1000}
+hash = {:iphone => 300, :samsung => 555, :nokia => 1000}
 hash.each.collect do |price| 
   puts "this phone is #{price}"
 end
