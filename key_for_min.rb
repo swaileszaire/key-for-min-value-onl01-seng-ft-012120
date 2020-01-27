a7f6baca3c65ed5e84faaab3132d57a05a6872ca
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-27 04:52:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hash)
+hash = {:iphone => 300, :samsung => 555, :nokia => 1000}
 
+key_for_the_min_value(hash)
 end
